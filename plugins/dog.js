1f3f73ebba7d32ca95897f4c73c072a48d2c6657
Queen Diana <dianadenzyy@gmail.com>
2025-06-29 14:43:44 -0400

    Actualizar dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,9 +14,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© 𝕊𝕚𝕝𝕧𝕒 𝕊𝕡𝕒𝕣𝕜 𝕄𝔻 ' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© 𝐃𝐈𝐀𝐍𝐀 ~ 𝐌𝐃 ' }, { quoted: mek });
     } catch (e) {
-        console.log(e); // ❯❯ 𝕊𝕚𝕝𝕧𝕒 𝕊𝕡𝕒𝕣𝕜 𝕄𝔻
+        console.log(e); // ❯❯ Diana MD
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
     }
 });
