9accd95351b9e9a9bc8aed00275cbd59bfb1d47c
sesco001 <marcasmatheka@gmail.com>
2025-07-03 15:00:14 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '🧝‍♂️𝐃𝐢𝐚𝐍𝐚 𝐃𝐞𝐯🧝‍♂️',
+  author: '‎',
+  botName: "DIANA-XMD",
+  botOwner: '🧝‍♂️𝐃𝐢𝐚𝐍𝐚 𝐃𝐞𝐯🧝‍♂️', // Your name
+  ownerNumber: '18492785095', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
