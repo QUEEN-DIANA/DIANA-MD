25473b8baae35f46927942a109770a3d3f17d2d9
Queen Diana <dianadenzyy@gmail.com>
2025-07-03 21:39:43 -0400

    Actualizar help.js

--- commands/help.js
+++ commands/help.js
@@ -147,7 +147,7 @@ async function helpCommand(sock, chatId, channelLink) {
 POWERED BY DIANA 🌍:`;
 
     try {
-        const imagePath = path.join(__dirname, '../assets/DIANA-MD.jpg');
+        const imagePath = path.join(__dirname, '../assets/bot_image.jpg');
         
         if (fs.existsSync(imagePath)) {
             const imageBuffer = fs.readFileSync(imagePath);
